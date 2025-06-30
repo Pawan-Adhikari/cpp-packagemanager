1e3b2a643c922ed1f7e99a68e0a48cfe38802528
Pawan-Adhikari <pawan.adk7@gmail.com>
2025-06-30 14:31:16 +0545

    Completed the init command

--- src/Project.cpp
+++ src/Project.cpp
@@ -15,15 +15,23 @@ void Project::initialize() {
     std::string mkdirCmd = "mkdir -p " + name;
     std::system(mkdirCmd.c_str());
     
-    std::string srcDir = "mkdir -p " + name + "/src";
+    std::string srcDir = mkdirCmd + "/src";
     std::system(srcDir.c_str());
     
-    std::string includeDir = "mkdir -p " + name + "/include";
+    std::string includeDir = mkdirCmd + "/include";
     std::system(includeDir.c_str());
     
-    std::string includepkg = "mkdir -p " + name + "/.cppkg";
+    std::string includepkg = mkdirCmd + "/.cppkg";
     std::system(includepkg.c_str());
 
+    std::string copy_main = "cp ../template/main_to_copy.cpp " + name + "/src/main.cpp";
+    std::system(copy_main.c_str());
+
+    std::string copy_makefile = "cp ../template/CMakeLists.txt " + name;
+    std::system(copy_makefile.c_str());
+
+    std::string copy_packagestxt = "cp ../template/packages.txt " + name + "/.cppkg/";
+    std::system(copy_packagestxt.c_str());
 
     std::cout << "Project structure created successfully!" << std::endl;
 }
